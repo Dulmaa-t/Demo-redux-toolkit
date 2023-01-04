ae284a4a0aefbdf06bb959729c108b4958fae3d2
Dulmaa-t <duka9705@gmail.com>
2023-01-04 13:22:50 +0800

    first commit

---
+++ components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, Fragment } from "react";
+
+const Footer = ({}) => {
+    return (
+            <footer>footer</footer>
+    )
+}
+
+export default Footer;
---
+++ components/layout/Header.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect, Fragment } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = ({ }) => {
+    return (
+        <header>header</header>
+    )
+}
+
+export default Header;
